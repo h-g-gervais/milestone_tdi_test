b97103435ea4aa536aef397b87862013fc12430a
h-g-gervais <hualong.gervais@gmail.com>
2017-09-08 16:30:44 -0400

    added placeholder plot

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template, request, redirect
+from bokeh.embed import components
+from bokeh.plotting import figure
 
 app = Flask(__name__)
 
+def create_figure():
+    x = [1, 2, 3, 4, 5]
+    y = [6, 7, 2, 4, 5]
+    
+    p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+    p.line(x, y, legend="Temp.", line_width=2)
+    
+    return p
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
@@ -11,7 +21,10 @@ def index():
         #my_app_lulu.prices = request.form.getlist('prices')
         #stock = request.form['ticker']
         #create_plot(stock, my_app_lulu.prices)
-        return render_template('plot_stocks.html')
+        
+        plot = create_figure()
+        script, div = components(plot)
+        return render_template('plot_stocks.html', script = script, div = div)
 
 
 if __name__ == '__main__':
